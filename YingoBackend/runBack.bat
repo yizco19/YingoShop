14351f1ffd73d2ca6714bdda0f0226441e784c7d
Yang <71834343+yizco19@users.noreply.github.com>
2024-03-25 16:41:05 +0100

    Delete YingoBackend directory

--- YingoBackend/runBack.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Iniciar Redis
-start "Redis Server" ".\Redis-x64-3.2.100\redis-server.exe" ".\Redis-x64-3.2.100\redis.windows.conf"
-
-rem Esperar unos segundos para que Redis se inicie completamente
-timeout /t 5 /nobreak >nul
-
-rem Iniciar el backend (reemplaza "nombre_del_archivo.jar" con el nombre real de tu archivo JAR)
-java -jar servidor_shop-1.0-SNAPSHOT.jar
