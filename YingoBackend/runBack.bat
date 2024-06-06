c8c57e94a272d28e834dc1bd22fd8f044ac26acb
Yang <71834343+yizco19@users.noreply.github.com>
2024-06-06 13:55:37 +0200

    Add files via upload

--- YingoBackend/runBack.bat
+++ YingoBackend/runBack.bat
@@ -1,22 +1,13 @@
 @echo off
 
-rem Verificar si el archivo de configuración de ngrok ya existe
-if not exist %USERPROFILE%\.ngrok2\ngrok.yml (
-    rem Configurar el token de autenticación de ngrok solo si el archivo de configuración no existe
-    ngrok authtoken <TU_TOKEN_DE_AUTENTICACIÓN>
-)
 
-rem Iniciar Redis
-start "Redis Server" ".\Redis-x64-3.2.100\redis-server.exe" ".\Redis-x64-3.2.100\redis.windows.conf"
+rem Iniciar Redis en una nueva ventana
+start "" /min ".\Redis-x64-3.2.100\redis-server.exe" ".\Redis-x64-3.2.100\redis.windows.conf"
 
-rem Esperar unos segundos para que Redis se inicie completamente
-timeout /t 5 /nobreak >nul
 
-rem Ejecutar ngrok para exponer el servidor local en el dominio de ngrok
-start "ngrok" ngrok http --domain=bonefish-unbiased-quickly.ngrok-free.app 8080
+rem Iniciar ngrok en una nueva ventana
+start "" /min ngrok.exe http --domain=bonefish-unbiased-quickly.ngrok-free.app 8080
 
-rem Esperar unos segundos para que ngrok se inicie completamente
-timeout /t 5 /nobreak >nul
 
 rem Iniciar el backend
 java -jar servidor_shop-1.0-SNAPSHOT.jar
